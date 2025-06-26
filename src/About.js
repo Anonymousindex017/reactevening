df73c15272a355fc2a6fde3a285a067001d4ded2
shashi <shashiweb64@gmail.com>
2025-06-26 19:44:05 +0530

    create new project

---
+++ src/About.js
@@ -0,0 +1,22 @@
+function Aboutpage()
+{
+    return <h1>this is heading</h1>
+}
+export default Aboutpage
+
+export function About1()
+{
+    return <p>this is name export function</p>
+}
+
+export function Abc()
+{
+    return <h3>this is heading three</h3>
+}
+
+
+export function Abcd1()
+{
+    return <h4>this is name export1 </h4>
+}
+
