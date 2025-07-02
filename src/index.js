064ae5f53f8e225549170d111eeabda8713cdcb3
shashi <shashiweb64@gmail.com>
2025-07-02 19:22:07 +0530

    proversion

--- src/index.js
+++ src/index.js
@@ -2,21 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-import "./style.css";
-import Xyz,{About1,Abcd1} from './About';
-import Contact from './Contact';
-import Mybootstrappage from './Mybootstrappage';
-import Hookspage from './Hookspage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hookspage></Hookspage>
-    <Xyz></Xyz>
-    <About1></About1>
-    <Abcd1/>
-    <Contact></Contact>
-    <Mybootstrappage></Mybootstrappage>
+
  
   </React.StrictMode>
 );
